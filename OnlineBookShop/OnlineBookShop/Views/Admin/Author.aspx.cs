8f01b18cf40864537f6ff755fdb4fcfbb10036be
trombone8417 <trombone8417@yahoo.com>
2024-07-26 13:30:03 +0800

    Author CRUD

--- OnlineBookShop/OnlineBookShop/Views/Admin/Author.aspx.cs
+++ OnlineBookShop/OnlineBookShop/Views/Admin/Author.aspx.cs
@@ -9,14 +9,127 @@ namespace OnlineBookShop.Views.Admin
 {
     public partial class Author : System.Web.UI.Page
     {
+        Models.Functions Con;
         protected void Page_Load(object sender, EventArgs e)
         {
+            Con = new Models.Functions();
+            ShowAuthors();
+        }
 
+        private void ShowAuthors()
+        {
+            string Query = "Select * from AuthorTbl";
+            AuthorsList.DataSource = Con.GetData(Query);
+            AuthorsList.DataBind();
         }
 
         protected void SaveBtn_Click(object sender, EventArgs e)
         {
+            try
+            {
+                if (ANameTb.Value == "" || GenCb.SelectedIndex == -1 || CountryCb.SelectedIndex == -1)
+                {
+                    ErrMsg.Text = "missing data";
+                }
+                else
+                {
+                    string AName = ANameTb.Value;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Country = CountryCb.SelectedItem.ToString();
 
+                    string Query = "insert into AuthorTbl values('{0}','{1}','{2}')";
+                    Query = string.Format(Query, AName, Gender, Country);
+                    Con.SetData(Query);
+                    ShowAuthors();
+                    ErrMsg.Text = "Author Inserted!!!";
+                    ANameTb.Value = "";
+                    GenCb.SelectedIndex = -1;
+                    CountryCb.SelectedIndex = -1;
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                ErrMsg.Text = Ex.Message;
+            }
+        }
+
+        int Key = 0;
+        protected void AuthorsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ANameTb.Value = AuthorsList.SelectedRow.Cells[2].Text;
+            GenCb.SelectedItem.Value = AuthorsList.SelectedRow.Cells[3].Text;
+            CountryCb.SelectedItem.Value = AuthorsList.SelectedRow.Cells[4].Text;
+            if (ANameTb.Value =="")
+            {
+                Key = 0;
+            }
+            else
+            {
+                Key = Convert.ToInt32(AuthorsList.SelectedRow.Cells[1].Text);
+            }
+        }
+
+        protected void EditBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ANameTb.Value == "" || GenCb.SelectedIndex == -1 || CountryCb.SelectedIndex == -1)
+                {
+                    ErrMsg.Text = "select a data";
+                }
+                else
+                {
+                    string AName = ANameTb.Value;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Country = CountryCb.SelectedItem.ToString();
+
+                    string Query = "update AuthorTbl set AutName='{0}',AutGender = '{1}', AutCountry = '{2}' where AutId = {3}";
+                    Query = string.Format(Query, AName, Gender, Country, AuthorsList.SelectedRow.Cells[1].Text);
+                    Con.SetData(Query);
+                    ShowAuthors();
+                    ErrMsg.Text = "Author Updated!!!";
+                    ANameTb.Value = "";
+                    GenCb.SelectedIndex = -1;
+                    CountryCb.SelectedIndex = -1;
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                ErrMsg.Text = Ex.Message;
+            }
+        }
+
+        protected void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (ANameTb.Value == "" || GenCb.SelectedIndex == -1 || CountryCb.SelectedIndex == -1)
+                {
+                    ErrMsg.Text = "select a Author";
+                }
+                else
+                {
+                    string AName = ANameTb.Value;
+                    string Gender = GenCb.SelectedItem.ToString();
+                    string Country = CountryCb.SelectedItem.ToString();
+
+                    string Query = "delete from AuthorTbl  where AutId = {0}";
+                    Query = string.Format(Query, AuthorsList.SelectedRow.Cells[1].Text);
+                    Con.SetData(Query);
+                    ShowAuthors();
+                    ErrMsg.Text = "Author Deleted!!!";
+                    ANameTb.Value = "";
+                    GenCb.SelectedIndex = -1;
+                    CountryCb.SelectedIndex = -1;
+                }
+            }
+            catch (Exception Ex)
+            {
+
+                ErrMsg.Text = Ex.Message;
+            }
         }
     }
 }
